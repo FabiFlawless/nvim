4239d104244ec186c21de9ba67dcf2c51800b750
MW\Fabian <fs@meso.biz>
2024-05-10 11:05:20 +0200

    installed lsp

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -1,6 +1,28 @@
 return {
+  {
     "williamboman/mason.nvim",
     config = function()
       require("mason").setup()
     end
+  },
+  {
+    "williamboman/mason-lspconfig.nvim",
+    config = function()
+      require("mason-lspconfig").setup({
+        ensure_installed = { 
+          "lua_ls", "bashls", "csharp_ls", "clangd",
+          "cmake", "cssls", "cssmodules_ls", "dockerls", 
+          "gopls", "jsonls", 
+          "biome", "marksman", "intelephense", "powershell_es", "sqls"
+        }
+      })
+    end
+  },
+  {
+    "neovim/nvim-lspconfig",
+    config = function()
+      local lspconfig = require("lspconfig")
+      lspconfig.lua_ls.setup({})
+    end
+  }
 }
