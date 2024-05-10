e05212cf292e28c0a2553fed18fef168a5074ffb
MW\Fabian <fs@meso.biz>
2024-05-10 15:47:19 +0200

    autocompletion added

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -39,13 +39,26 @@ return {
 	{
 		"neovim/nvim-lspconfig",
 		config = function()
+			local capabilities = require("cmp_nvim_lsp").default_capabilities()
 			local lspconfig = require("lspconfig")
-			lspconfig.lua_ls.setup({})
-			lspconfig.cssls.setup({})
-			lspconfig.csharp_ls.setup({})
-			lspconfig.gopls.setup({})
-			lspconfig.biome.setup({})
-			lspconfig.powershell_es.setup({})
+			lspconfig.lua_ls.setup({
+				capabilities = capabilities,
+			})
+			lspconfig.cssls.setup({
+				capabilities = capabilities
+			})
+			lspconfig.csharp_ls.setup({
+				capabilities = capabilities
+			})
+			lspconfig.gopls.setup({
+				capabilities = capabilities
+			})
+			lspconfig.biome.setup({
+				capabilities = capabilities
+			})
+			lspconfig.powershell_es.setup({
+				capabilities = capabilities
+			})
 			vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
 			vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
 			vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, {})
